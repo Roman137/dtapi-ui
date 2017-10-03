4e8cbe8b789a658a88133b37792278a4c80578a5
Roman137 <roman.pindak@gmail.com>
2017-10-03 14:39:45 +0300

    [ADDED] Angular maetrial module [DOWNGRADED] Package dependencies to stable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MyOwnCustomMaterialModule} from '../custom-material-module/my-own-custom-material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MyOwnCustomMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
